f3bffd8bedc31413ab72c57a340d7bf04dbd50eb
T Bambang A <dasci.ebook@gmail.com>
2020-06-05 01:59:35 +0100

    Add Address table

--- address_book/urls.py
+++ address_book/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
 	path('', views.home, name="home"),
 	path('add_address', views.add_address, name="add_address"),
+	path('edit/<list_id>', views.edit, name="edit"),
+	path('delete/<list_id>', views.delete, name="delete"),
 ]
